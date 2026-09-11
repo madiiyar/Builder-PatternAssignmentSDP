3acde233adc31c8feb31d0f684c857342e66829c
Madiyar <aqylbek.madiyar@gmail.com>
2026-09-11 11:06:22 +0500

    created builder interfacae

---
+++ src/TravelPackageBuilder.java
@@ -0,0 +1,5 @@
+public interface TravelPackageBuilder {
+    TravelPackageBuilder setDestination(String destination);
+    TravelPackageBuilder setFlight(String flight);
+    TravelPackageBuilder setHotel(String hotel);
+}
